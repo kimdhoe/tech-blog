2452d3210e48a04225bfc3e4551a05ae15e5ad46
Donghee Kim <kimdhoe1@gmail.com>
2019-08-02 18:11:35 +0900

    bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,8 +53,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 }
