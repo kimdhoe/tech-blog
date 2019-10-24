c89c9f04bf68c0112180510aa3077592c676655e
Donghee Kim <kimdhoe1@gmail.com>
2019-10-24 22:22:25 +0900

    feat: set Sentry up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,11 @@
+import * as Sentry from '@sentry/browser'
 require('./static/styles/code-theme.css')
 require('typeface-noto-sans-kr')
+
+export const onClientEntry = (_, pluginParams) => {
+  if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+    })
+  }
+}
