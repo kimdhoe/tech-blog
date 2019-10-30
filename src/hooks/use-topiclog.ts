27ff0c2d3364e5140f7d3d9e819af4a8c36a0ed0
Donghee Kim <kimdhoe1@gmail.com>
2019-10-30 12:14:57 +0900

    feat: topic log template

---
+++ src/hooks/use-topiclog.ts
@@ -0,0 +1,60 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImageSharpFluidWithWebp } from '../types'
+
+interface TopicLog {
+  slug: string
+  title: string
+  date: string
+  dateFormatted: string
+}
+
+interface Data {
+  allContentfulTopicLog: {
+    nodes: TopicLog[]
+  }
+  image: {
+    childImageSharp: {
+      fluid: GatsbyImageSharpFluidWithWebp
+    }
+  }
+}
+
+const useTopicLog = (): {
+  topicLogs: TopicLog[]
+  image: GatsbyImageSharpFluidWithWebp | null
+} => {
+  const data: Data = useStaticQuery(graphql`
+    query {
+      allContentfulTopicLog(sort: { fields: updateTime, order: DESC }) {
+        nodes {
+          title
+          slug
+          date: updateTime
+          dateFormatted: updateTime(formatString: "dddd, MMMM D, YYYY")
+        }
+      }
+      image: file(
+        sourceInstanceName: { eq: "images" }
+        relativePath: { eq: "eastwood-downloading.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return {
+    topicLogs: data.allContentfulTopicLog.nodes.map(node => ({
+      slug: node.slug,
+      title: node.title,
+      date: node.date,
+      dateFormatted: node.dateFormatted,
+    })),
+    image: data.image ? data.image.childImageSharp.fluid : null,
+  }
+}
+
+export default useTopicLog
--- src/pages/topiclog.tsx
+++ src/pages/topiclog.tsx
@@ -4,13 +4,17 @@ import { css } from '@emotion/core'
 
 import { SEO } from '../components/seo'
 import { PageHeader } from '../components/page-header'
+import useTopicLog from '../hooks/use-topiclog'
 
 interface TopicPreviewProps {
+  slug: string
   title: string
   update: string
 }
 
 const TopicLog: React.FunctionComponent = () => {
+  const { topicLogs, image } = useTopicLog()
+
   return (
     <div css={styles.container}>
       <SEO
@@ -18,11 +22,18 @@ const TopicLog: React.FunctionComponent = () => {
         description={'Records left in chronological order'}
       />
       <div css={styles.header}>
-        <PageHeader headline="Topic Log" />
+        <PageHeader headline="Topic Log" image={image} />
       </div>
       <div css={styles.wrapper}>
         <ul css={styles.topics}>
-          <TopicPreview title={'TypeScript'} update={'October 29, 2019'} />
+          {topicLogs.map(topicLog => (
+            <TopicPreview
+              key={topicLog.slug}
+              slug={topicLog.slug}
+              title={topicLog.title}
+              update={topicLog.dateFormatted}
+            />
+          ))}
         </ul>
       </div>
     </div>
@@ -30,15 +41,16 @@ const TopicLog: React.FunctionComponent = () => {
 }
 
 const TopicPreview: React.FunctionComponent<TopicPreviewProps> = ({
+  slug,
   title,
   update,
 }) => {
   return (
     <li css={styles.topic}>
-      <Link css={styles.topicLink} to={'/topiclog/typescript-react'}>
+      <Link css={styles.topicLink} to={`/topiclog/${slug}/`}>
         <p css={styles.topicTitle}>
           {title}
-          <span css={styles.topicUpdate}>{update}</span>
+          <span css={styles.topicUpdate}>Updated on {update}</span>
         </p>
       </Link>
     </li>
